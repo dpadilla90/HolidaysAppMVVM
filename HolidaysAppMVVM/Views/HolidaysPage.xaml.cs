7c897ebdd2ecba5a8513c6ad0344488d1ded1601
Dennis Padilla <d_padilla.y@hotmail.com>
2023-10-29 14:54:31 -0400

    Added HolidaysPage code-behind and adjusted ViewModel namespace in XAML.

---
+++ HolidaysAppMVVM/Views/HolidaysPage.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaysAppMVVM.Views
+{
+    public partial class HolidaysPage : ContentPage
+    {
+        public HolidaysPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
